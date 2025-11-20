532beb694a2dda08c2cf89ad9233591746f43b49
MielomankA <mielomanka@mail.ru>
2025-11-21 00:33:34 +0300

    feat: add resolvers for profile, user

--- src/routes/graphql/types/profile.ts
+++ src/routes/graphql/types/profile.ts
@@ -1,6 +1,16 @@
 import { GraphQLBoolean, GraphQLInt, GraphQLNonNull, GraphQLObjectType } from 'graphql';
 import { MemberType } from './member-type.js';
 import { UUIDType } from '../types/uuid.js';
+import { GQLContext } from '../types/gql-context.js';
+
+type MemberTypeId = 'BASIC' | 'BUSINESS';
+
+type ProfileModel = {
+  id: string;
+  isMale: boolean;
+  yearOfBirth: number;
+  memberTypeId: MemberTypeId;
+};
 
 export const Profile = new GraphQLObjectType({
   name: 'Profile',
@@ -8,6 +18,11 @@ export const Profile = new GraphQLObjectType({
     id: { type: new GraphQLNonNull(UUIDType) },
     isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
     yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
-    memberType: { type: new GraphQLNonNull(MemberType) },
+    memberType: {
+      type: new GraphQLNonNull(MemberType),
+      resolve: (profile: ProfileModel, _, ctx: GQLContext) => {
+        return ctx.prisma.memberType.findUnique({ where: { id: profile.memberTypeId } });
+      },
+    },
   }),
 });
--- src/routes/graphql/types/user.ts
+++ src/routes/graphql/types/user.ts
@@ -1,16 +1,66 @@
-import { GraphQLFloat, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
+import {
+  GraphQLFloat,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
 import { Post } from './post.js';
 import { Profile } from './profile.js';
+import { UUIDType } from '../types/uuid.js';
+import { GQLContext } from '../types/gql-context.js';
+import { GraphQLFieldConfigMap } from 'graphql';
 
-export const User = new GraphQLObjectType({
-    name: 'User',
-    fields: () => ({
-        id: { type: new GraphQLNonNull(GraphQLInt) },
-        name: { type: new GraphQLNonNull(GraphQLString) },
-        balance: { type: new GraphQLNonNull(GraphQLFloat) },
-        profile: { type: new GraphQLNonNull(Profile) },
-        posts: { type: new GraphQLNonNull(new GraphQLList(Post)) },
-        userSubscribedTo: { type: new GraphQLNonNull(new GraphQLList(User)) },
-        subscribedToUser: { type: new GraphQLNonNull(new GraphQLList(User)) },
-    }),
+type PostModel = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type UserModel = {
+  id: string;
+  name: string;
+  balance: number;
+  profileId?: string;
+  posts: PostModel[];
+  userSubscribedToIds: string[];
+  subscribedToUserIds: string[];
+};
+
+export const User = new GraphQLObjectType<UserModel, GQLContext>({
+  name: 'User',
+  fields: (): GraphQLFieldConfigMap<UserModel, GQLContext> => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    profile: { type: Profile },
+    posts: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(Post))),
+      resolve: (user, _, ctx) => {
+        return ctx.prisma.post.findMany({ where: { authorId: user.id } });
+      },
+    },
+    userSubscribedTo: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(User))),
+      resolve: async (user, _, ctx) => {
+        const relations = await ctx.prisma.subscribersOnAuthors.findMany({
+          where: { subscriberId: user.id },
+          include: { author: true },
+        });
+
+        return relations.map((r) => r.author);
+      },
+    },
+    subscribedToUser: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(User))),
+      resolve: async (user, _, ctx) => {
+        const relations = await ctx.prisma.subscribersOnAuthors.findMany({
+          where: { authorId: user.id },
+          include: { subscriber: true },
+        });
+
+        return relations.map((r) => r.subscriber);
+      },
+    },
+  }),
 });
